cde947a2b91848cc36f9e335fc89b2fe2161323e
Jan-Matthis <mail@jan-matthis.de>
2020-09-21 12:00:55 +0200

    Remove Pyro MCMC

--- tests/mcmc_slice_pyro/test_slice.py
+++ tests/mcmc_slice_pyro/test_slice.py
@@ -20,10 +20,9 @@ from pyro.contrib.conjugate.infer import (
 from pyro.infer import SVI, TraceEnum_ELBO
 from pyro.infer.autoguide import AutoDelta
 
-# from pyro.infer.mcmc.api import MCMC
+from pyro.infer.mcmc.api import MCMC
 from pyro.util import ignore_jit_warnings
 
-from sbi.mcmc.mcmc import MCMC
 from sbi.mcmc.slice import Slice
 
 from .common import assert_equal
