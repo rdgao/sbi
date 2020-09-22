48a3590d3b49b78c1fea848c0ccc80ae2b540e41
Jan-Matthis <mail@jan-matthis.de>
2020-09-22 21:41:41 +0200

    Vectorize SIR init

--- sbi/mcmc/init_strategy.py
+++ sbi/mcmc/init_strategy.py
@@ -12,7 +12,10 @@ def prior_init(prior: Any) -> Tensor:
 
 
 def sir(
-    prior: Any, potential_fn: Callable, init_strategy_num_candidates: int,
+    prior: Any,
+    log_prob_fn: Callable,
+    init_strategy_num_candidates: int = 10_000,
+    batch_size: int = 1_000,
 ) -> Tensor:
     r"""
     Return a sample obtained by sequential importance reweighing.
@@ -22,26 +25,32 @@ def sir(
 
     Args:
         prior: Prior distribution, candidate samples are drawn from it.
-        potential_fn: Potential function that the candidate samples are weighted with.
+        log_prob_fn: Log prob function that the candidate samples are weighted with.
         init_strategy_num_candidates: Number of candidate samples drawn.
+        batch_size: Batch size for evaluations
 
     Returns:
         A single sample.
     """
+    with torch.set_grad_enabled(False):
+        num_batches = int(init_strategy_num_candidates / batch_size)
 
-    init_param_candidates = prior.sample((init_strategy_num_candidates,)).detach()
+        log_weights = []
+        init_param_candidates = []
+        for i in range(num_batches):
+            batch_draws = prior.sample((batch_size,)).detach()
+            init_param_candidates.append(batch_draws)
+            log_weights.append(log_prob_fn(batch_draws.numpy()).detach())
+        log_weights = torch.cat(log_weights)
+        init_param_candidates = torch.cat(init_param_candidates)
 
-    log_weights = torch.cat(
-        [
-            potential_fn(init_param_candidates[i, :]).detach()
-            for i in range(init_strategy_num_candidates)
-        ]
-    )
-    probs = np.exp(log_weights.view(-1).numpy().astype(np.float64))
-    probs[np.isnan(probs)] = 0.0
-    probs[np.isinf(probs)] = 0.0
-    probs /= probs.sum()
-    idxs = np.random.choice(
-        a=np.arange(init_strategy_num_candidates), size=1, replace=False, p=probs,
-    )
-    return init_param_candidates[torch.from_numpy(idxs.astype(int)), :]
+        probs = np.exp(log_weights.view(-1).numpy().astype(np.float64))
+        probs[np.isnan(probs)] = 0.0
+        probs[np.isinf(probs)] = 0.0
+        probs /= probs.sum()
+
+        idxs = np.random.choice(
+            a=np.arange(init_strategy_num_candidates), size=1, replace=False, p=probs,
+        )
+
+        return init_param_candidates[torch.from_numpy(idxs.astype(int)), :]
